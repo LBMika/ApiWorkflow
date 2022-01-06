0166d8d094026978b38ad3f216c313123153c48c
LMika <lemika.pro@gmail.com>
2022-01-06 15:34:29 +0100

    Add Yeha !

--- src/main/java/fr/semifir/testDemo/employes/EmployeController.java
+++ src/main/java/fr/semifir/testDemo/employes/EmployeController.java
@@ -25,6 +25,7 @@ public class EmployeController {
 
     @GetMapping("{id}")
     public ResponseEntity<EmployeDTO> findById(@PathVariable Long id) {
+        System.out.println("Yeha !!!!!!!!!");
         try {
             Optional<EmployeDTO> employeDTO = this.service.findById(id);
             return ResponseEntity.ok(employeDTO.get());
